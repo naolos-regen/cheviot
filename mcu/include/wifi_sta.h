440d80569c5f9adbb841ce6fb482f5edaf43d368
naolos <naolos.regen@proton.me>
2026-01-25 00:34:09 +0100

    remove blob

--- mcu/include/wifi_sta.h
+++ mcu/include/wifi_sta.h
@@ -2,15 +2,11 @@
 #define MCU_WIFI_STA_H
 
 #include <esp_wifi.h>
-#include <esp_wifi_types_generic.h>
 #include <credentials.h>
 #include <esp_event_base.h>
 #include <esp_log.h>
 
 #define MAX_RETRY 10
-
-#define WIFI_CONNECTED_BIT BIT1
-#define WIFI_FAIL_BIT	   BIT0
 
 extern void wifi_init_sta();
 extern void event_handler(void *, esp_event_base_t, int32_t, void *);
